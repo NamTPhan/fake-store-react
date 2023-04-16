3f0c974f9ba85b6fe7ea9a1067fc78e4eb5f1f98
Nam <namtphann@gmail.com>
2023-04-16 16:11:44 +0200

    Add redux setup and create store with theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const container =
   document.getElementById("root") || document.createElement("div");
 
 const root = ReactDOMClient.createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 localStorage.setItem("theme", "light");
 reportWebVitals(console.log);
